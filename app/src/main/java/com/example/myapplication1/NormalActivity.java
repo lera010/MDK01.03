4ba5df02f73038cc53901b5e53836b8e1af2b9c4
leray <lerayudanova05@gmail.com>
2025-01-31 15:09:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/NormalActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NormalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.normal_layout);
+    }
+}
